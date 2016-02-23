0763ba7c449249d4f245241529b2153ec412fbf9
ArchX1-erik <elegenstein@student.tgm.ac.at>
2016-02-23 21:58:24 +0100

    Station is 80 % implemented jdbc to pg is missing && RXTHREAD

--- src/main/java/ebraendli/Station.java
+++ src/main/java/ebraendli/Station.java
@@ -1,7 +1,12 @@
 package ebraendli;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
 
 /**
@@ -13,6 +18,9 @@ public class Station {
     private boolean isReady= true;
     private boolean isListening = true;
     private boolean hasCurrentSql = false;
+    private Statement stmt;
+    private Connection con;
+    private PrintWriter logger;
 
 
     public Station(String IpOfTransManager){
@@ -26,44 +34,72 @@ public class Station {
                 e.printStackTrace();
             }
         logFile = f.getName();
+        try {
+            logger = new PrintWriter(new FileWriter(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void parseInput(String rxMsg){
         if (rxMsg.equals("abort;")){
+            logger.write(String.format("%td-%tm-%ty Doing Rollback...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
             doRollback();
             return;
         }
         if (rxMsg.equals("commit;")){
+            logger.write(String.format("%td-%tm-%ty Doing Commit...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
             doCommit();
             return;
         }
+        logger.write(String.format("%td-%tm-%ty Got new Transaction...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
         doSql(rxMsg);
     }
 
     private void doRollback(){
-        //todo implement
+        try {
+            con.rollback();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.print(e.getMessage());
+        }
         this.hasCurrentSql = false;
     }
 
     private void doCommit(){
-        //todo implement
+        try {
+            con.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.print(e.getMessage());
+        }
         this.hasCurrentSql = false;
     }
 
     private void callFailed(){
+        logger.write(String.format("%td-%tm-%ty Calling Failed...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
         ComIPC.send(ipTransMan,ConstraintsAndUtils.COM_PORT,"failed;");
     }
 
     private void callReady(){
+        logger.write(String.format("%td-%tm-%ty Calling Ready...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
         ComIPC.send(ipTransMan,ConstraintsAndUtils.COM_PORT,"ready;");
     }
 
-    private void doSql(String sql){
+    private void doSql(String sql) {
         if (this.hasCurrentSql) {
             callFailed();
             return;
         }
-        //todo implement
+        try {
+            con.setAutoCommit(false);
+            stmt = con.createStatement();
+            stmt.execute(sql);
+            logger.print(String.format("%td-%tm-%ty Waiting for Commit...",Calendar.getInstance(),Calendar.getInstance(),Calendar.getInstance()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //todo logging
+        }
         this.hasCurrentSql = true;
         callReady();
     }
